a9ab41dd11e1b4bc1a7677a46305bcde6b587207
Paul Kimani <pabrokimani@gmail.com>
2023-04-10 14:23:25 +0300

    Add basic math operators

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//functions for basic math operators
+function add(a, b) {
+    return a + b; 
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
